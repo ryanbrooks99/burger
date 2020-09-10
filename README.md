a9e5473741c58d20b65dfd10a6c3bf132b0715d8
ryanbrooks99 <ryanbrooks99@gmail.com>
2020-09-09 22:16:30 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # EAT-DA-BURGER!
 
-## 
+## This is a burger-generating application which utilizes MySQL, Node, Express, Handlebars, and an ORM. The application allows you to input your favorite burger then devour it!
+
+![Eat-Da-Burger!](public/assets/images/eatdaburger.png)
+
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,10 +2,9 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
-// Create all our routes and set up logic within those routes where required.
+// Create all our routes and set up logic within those routes where required
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
@@ -22,7 +21,7 @@ router.post("/api/burgers", function(req, res) {
   ], [
     req.body.burger_name, req.body.devoured
   ], function(result) {
-    // Send back the ID of the new quote
+    
     res.json({ id: result.insertId });
   });
 });
@@ -36,7 +35,6 @@ router.put("/api/burgers/:id", function(req, res) {
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -44,5 +42,4 @@ router.put("/api/burgers/:id", function(req, res) {
   });
 });
 
-// Export routes for server.js to use.
 module.exports = router;
